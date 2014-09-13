3e445a664d5a8fbd6fa6f66972316bda275f0e33
Eric Typaldos <etypaldos@pappasgroup.com>
2014-09-13 11:23:15 -0400

    Stopping point before form handling.Page tests are broken for some reason still

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ var app = express();
 
 // Set up handlebars as our template engine
 var handlebars = require('express3-handlebars');
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({
+	defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 // Set up static dir
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
+
+// Add middleware for testing
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
 
 var fortune = require('./lib/fortune');
 
@@ -19,9 +27,23 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-	res.render('about', { fortune: fortune.getFortune() });
+	res.render('about', {
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
 });
 
+app.get('/tours/hood-river', function (req, res) {
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function (req, res) {
+	res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+	res.render('tours/request-group-rate');
+});
 
 
 // ------------------------------
