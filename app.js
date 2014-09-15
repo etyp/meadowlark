d33bc4ccc45e5ba7d938160e5e475563af9e5dbf
Eric Typaldos <etypaldos@pappasgroup.com>
2014-09-15 16:37:11 -0400

    Testing git through sublime text

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 var express = require('express');
+var http = require('http');
 var bodyParser = require('body-parser');
 var formidable = require('formidable');
+var nodemailer = require('nodemailer');
+
+// Init app
 var app = express();
+
+// Logging config (environment based)
+switch (app.get('env')) {
+	case 'development':
+		app.use(require('morgan')('dev'));
+		break;
+	case 'production':
+		// express-logger module
+		app.use(require('express-logger')({
+			path: __dirname + '/log/requests.log'
+		}));
+		break;
+}
+
+// Local modules
+var fortune = require('./lib/fortune');
+var credentials = require('./credentials');
 
 // Set up bodyParser
 app.use(bodyParser());
@@ -31,10 +52,23 @@ app.use(function(req, res, next) {
 	next();
 });
 
-var fortune = require('./lib/fortune');
+// Middleware for cookie parsing
+app.use(require('cookie-parser')(credentials.cookieSecret));
+// Middleware for sessions
+app.use(require('express-session')());
+
+// Flash message middleware
+app.use(function(req, res, next) {
+	// if we have a flash msg, move it to local context & clear it
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
+
 
 // Routes ===================================
 app.get('/', function(req, res) {
+	res.cookie('eric', 'rah rah rah i was here');
 	res.render('home');
 });
 
@@ -57,29 +91,32 @@ app.get('/tours/request-group-rate', function(req, res) {
 	res.render('tours/request-group-rate');
 });
 
-app.get('/newsletter', function (req, res) {
-	res.render('newsletter', { csrf: 'CSRF token here!' });
+app.get('/newsletter', function(req, res) {
+	res.render('newsletter', {
+		csrf: 'CSRF token here!'
+	});
 });
 
-app.post('/process', function (req, res) {
+app.post('/process', function(req, res) {
 	if (req.xhr || req.accepts('json, html') === 'json') {
-		res.send({success: true});
-	}
-	else {
-		console.log("Form querystring: "+req.query.form);
-		console.log("CSRF token: "+req.body._csrf);
-		console.log("Name field: "+req.body.name);
-		console.log("Email field: "+req.body.email);
+		res.send({
+			success: true
+		});
+	} else {
+		console.log("Form querystring: " + req.query.form);
+		console.log("CSRF token: " + req.body._csrf);
+		console.log("Name field: " + req.body.name);
+		console.log("Email field: " + req.body.email);
 		res.redirect(303, '/thank-you');
 	}
 });
 
-app.get('/thank-you', function (req, res) {
+app.get('/thank-you', function(req, res) {
 	res.render("thank-you");
 });
 
 // File upload route handlers
-app.get('/contest/vacation-photo', function (req, res) {
+app.get('/contest/vacation-photo', function(req, res) {
 	var now = new Date();
 	res.render('contest/vacation-photo', {
 		year: now.getFullYear(),
@@ -87,10 +124,10 @@ app.get('/contest/vacation-photo', function (req, res) {
 	});
 });
 
-app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
 	var form = new formidable.IncomingForm();
-	form.parse(req, function (err, fields, files) {
-		if(err) return res.redirect(303, '/error');
+	form.parse(req, function(err, fields, files) {
+		if (err) return res.redirect(303, '/error');
 		console.log("received fields: ");
 		console.log(fields);
 		console.log("received files: ");
@@ -114,8 +151,20 @@ app.use(function(err, req, res, next) {
 	res.render('500');
 });
 
+function startServer() {
+	// Create http server, listen on port
+	http.createServer(app).listen(app.get('port'), function() {
+		console.log("App started in " + app.get('env') + " env. Listening on port " + app.get('port'));
+	});
+}
 
-// Create http server, listen on port
-app.listen(app.get('port'), function() {
-	console.log("Listening on port " + app.get('port'));
-});
+if (require.main === module) {
+	// application run directly, start app server
+	startServer();
+}
+else {
+	// application imported as a module via require.
+	// Export function to create server
+	module.exports = startServer;
+}
+
